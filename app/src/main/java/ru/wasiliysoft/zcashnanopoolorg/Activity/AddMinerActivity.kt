96aab13e92d445a52f85e992763f2e7f247681c4
WasiliySoft <wasiliysoft@gmail.com>
2019-01-20 17:00:26 +0500

    addTextChangedListener

--- app/src/main/java/ru/wasiliysoft/zcashnanopoolorg/Activity/AddMinerActivity.kt
+++ app/src/main/java/ru/wasiliysoft/zcashnanopoolorg/Activity/AddMinerActivity.kt
@@ -1,72 +1,67 @@
 package ru.wasiliysoft.zcashnanopoolorg.Activity
 
-import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.fragment_add_miner.*
 import ru.wasiliysoft.zcashnanopoolorg.Model.Miner
 import ru.wasiliysoft.zcashnanopoolorg.Model.Miners
 import ru.wasiliysoft.zcashnanopoolorg.R
+import java.util.regex.Pattern
 
 class AddMinerActivity : AppCompatActivity(), View.OnClickListener {
 
-    private var etName: EditText? = null
-    private var etAddress: EditText? = null
+
     private var miners: Miners? = null
     private val TAG = "MainActivity2"
-    private var data: Uri? = null
-    private var tvAccount: TextView? = null
-    private var tvTicker: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_add_miner)
         miners = Miners(applicationContext)
+        bAddMiner.setOnClickListener(this)
+        qrBtn.setOnClickListener(this)
 
-        etName = findViewById(R.id.minerName)
-        etAddress = findViewById(R.id.minerUrlAddress)
-        tvAccount = findViewById(R.id.accountTextView)
-        tvTicker = findViewById(R.id.tickerTextView)
-        data = intent.data
-        if (data != null) {
-            val params = data!!.pathSegments
-            etAddress!!.setText(params[1])
-        }
-        (findViewById<View>(R.id.bAddMiner) as Button).setOnClickListener(this)
-        (findViewById<View>(R.id.qrBtn) as Button).setOnClickListener(this)
+        minerUrlAddress.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                updateMinerPrefUi(p0.toString())
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
     }
+
 
     override fun onClick(view: View) {
         when (view.id) {
             R.id.bAddMiner -> {
 
-                if (etName!!.text.toString().trim { it <= ' ' }.isEmpty()) {
+                if (minerName.text.toString().trim().isEmpty()) {
                     return
                 }
-                if (tvTicker!!.text.toString().trim { it <= ' ' }.isEmpty()) {
+                if (tickerTextView.text.toString().trim().isEmpty()) {
                     return
                 }
-                if (tvAccount!!.text.toString().trim { it <= ' ' }.isEmpty()) {
+                if (accountTextView.text.toString().trim().isEmpty()) {
                     return
                 }
-                val m = Miner(etName!!.text.toString().trim { it <= ' ' }, tvTicker!!.text.toString().trim { it <= ' ' }, tvAccount!!.text.toString().trim { it <= ' ' })
+                val m = Miner(minerName.text.toString().trim(), tickerTextView.text.toString().trim(), accountTextView.text.toString().trim())
                 miners!!.add(m)
-                setResult(Activity.RESULT_OK)
-                if (data != null) {
-                    val i = Intent(this, MainActivity::class.java)
-                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                    startActivity(i)
-                }
-                finish()
 
+                val i = Intent(this, MainActivity::class.java)
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK) //todo надо уточнить
+                startActivity(i)
                 return
             }
             R.id.qrBtn -> {
@@ -74,6 +69,28 @@ class AddMinerActivity : AppCompatActivity(), View.OnClickListener {
                 val i = integrator.createScanIntent()
                 startActivityForResult(i, IntentIntegrator.REQUEST_CODE)
                 return
+            }
+        }
+    }
+
+    fun updateMinerPrefUi(url: String) {
+        val uri = Uri.parse(url)
+        val host = uri.host
+        if (host != null) {
+            val hostParams = host.split(regex = Pattern.compile("\\."))
+            Log.d(TAG, hostParams.size.toString())
+            if (hostParams.size >= 2) {
+                val ticker = hostParams[0]
+                Log.d(TAG, ticker)
+                val domain = hostParams[1]
+                Log.d(TAG, host)
+                if (domain.equals("nanopool")) {
+                    if (uri.pathSegments.size >= 2) {
+                        val account = uri.pathSegments[1]
+                        tickerTextView!!.text = ticker
+                        accountTextView!!.text = account
+                    }
+                }
             }
         }
     }
@@ -87,25 +104,9 @@ class AddMinerActivity : AppCompatActivity(), View.OnClickListener {
                 Log.d(TAG, "Barcode read: $re")
                 val scanedUrl = Uri.parse(re)
                 if (scanedUrl != null) {
-                    val host = scanedUrl.host
-                    val domain = host!!.split("\\.".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1]
-                    Log.d(TAG, host)
-
-                    Log.d(TAG, domain)
-                    if (domain == "nanopool") {
-                        etAddress!!.setText(scanedUrl.toString())
-                        val ticker = host.split("\\.".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0]
-                        Log.d(TAG, ticker)
-                        val account = scanedUrl.pathSegments[1]
-                        tvTicker!!.text = ticker
-                        tvAccount!!.text = account
-
-
-                    } else {
-                        Toast.makeText(this, getString(R.string.unsupported_url) + getString(R.string.host), Toast.LENGTH_LONG).show()
-                    }
+                    minerUrlAddress!!.setText(scanedUrl.toString())
                 } else {
-                    Toast.makeText(this, getString(R.string.unsupported_url) + getString(R.string.host), Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, getString(R.string.unsupported_url), Toast.LENGTH_LONG).show()
                 }
                 return
             }
